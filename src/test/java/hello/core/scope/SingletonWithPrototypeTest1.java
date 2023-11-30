8edd825d11ff9dccedec12e0e6c7f5ee6a4cea51
solarbear <sons1998@naver.com>
2023-11-30 19:55:18 +0900

    싱글톤과 프로로타입 Bean을 같이 사용할 때 문제점 해결방법(JSR-330 Provider)

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -43,13 +44,21 @@ public class SingletonWithPrototypeTest1 {
 
         //싱글톤 Bean에서 Prototype Bean 사용 시 새로운 빈을 계속 생성 해주기 위해서는
         //ObjectProvider나 ObjectFactory라는 것을 사용하면 됨
-        @Autowired
-        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+//        @Autowired
+//        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
         //ObjectFactory와 ObjectProvider 둘다 스프링에 의존적이다
         //ObjectProvider는 상속, 옵션 처리나 스트림 처리 등 편의기능이 많음.
 
+
+        //자바에서 제공하는 JSR-330 Provider
+        //자바에서 제공하기 때문에 스프링 컨테이너에서만 사용할 수 있는 것이 아닌 다른 컨테이너에서도 사용 가능함.
+        //하지만 라이브러리 추가 해야함.
+        @Autowired
+        private Provider<PrototypeBean> prototypeBeanProvider;
+
         public int logic() {
-            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+            //PrototypeBean prototypeBean = prototypeBeanProvider.getObject(); -> ObjectFactory나 ObjectProvider 사용할때 메서드
+            PrototypeBean prototypeBean = prototypeBeanProvider.get(); //자바에서 제공하는 JSR-330 Provider를 사용 할 때는 get()
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
